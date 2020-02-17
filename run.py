2a06022d3b2a4f3700b2afea9b6dbfb629634e20
Permyak-Logy <maxsika2004@mail.ru>
2020-02-17 17:02:29 +0400

    Задача 2 Добавить обработку клавиш PgUp и PgDown, по нажатию на которые соответственно увеличивать и уменьшать масштаб отображения карты. Необходимо отслеживать предельные значения, за которые значения переменных не должны заходить.

--- run.py
+++ run.py
@@ -3,7 +3,8 @@ import sys
 
 import requests
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel
+from PyQt5.QtCore import Qt
 
 SCREEN_SIZE = [600, 450]
 
@@ -14,9 +15,43 @@ class Example(QMainWindow):
         self.ll = "37.530887,55.703118"
         self.spn = "0.002,0.002"
         self.l = "map"
-
-        self.getImage()
         self.initUI()
+        self.update_image()
+
+    def update_image(self):
+        self.loading.show()
+        self.getImage()
+        self.pixmap = QPixmap(self.map_file)
+        self.image.setPixmap(self.pixmap)
+        self.loading.hide()
+
+    def keyPressEvent(self, event):
+        max_spn = 85.0
+        min_spn = 0.001
+        if event.key() == Qt.Key_Up:
+            delta = 0.5
+            data = list(map(float, self.spn.split(',')))
+            data[0] *= (1 + delta)
+            data[1] *= (1 + delta)
+            if data[0] > max_spn:
+                data[0] = max_spn
+
+            if data[1] > max_spn:
+                data[1] = max_spn
+            self.spn = ','.join(list(map(str, data)))
+            self.update_image()
+        elif event.key() == Qt.Key_Down:
+            delta = 0.5
+            data = list(map(float, self.spn.split(',')))
+            data[0] *= (1 - delta)
+            data[1] *= (1 - delta)
+            if data[0] < min_spn:
+                data[0] = min_spn
+            if data[1] < min_spn:
+                data[1] = min_spn
+            self.spn = ','.join(list(map(str, data)))
+            self.update_image()
+        print(self.spn)
 
     def getImage(self):
         map_api_server = "http://static-maps.yandex.ru/1.x/"
@@ -43,15 +78,26 @@ class Example(QMainWindow):
         self.setWindowTitle('Отображение карты')
 
         # Изображение
-        self.pixmap = QPixmap(self.map_file)
         self.image = QLabel(self)
         self.image.move(0, 0)
         self.image.resize(600, 450)
-        self.image.setPixmap(self.pixmap)
+
+        # Надпись загрузки
+        self.loading = LoadingText()
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
+
+
+class LoadingText(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUi()
+
+    def initUi(self):
+        self.setWindowTitle("Обновление...")
+        self.setFixedSize(300, 1)
 
 
 if __name__ == '__main__':
