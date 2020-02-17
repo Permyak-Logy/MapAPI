dfc1faf33ba1ff1fe9d7a08b10f887844b50c798
Permyak-Logy <maxsika2004@mail.ru>
2020-02-17 16:27:35 +0400

    Задача 1 Создать оконное приложение, отображающее карту по координатам в масштабе, который задается программно.

---
+++ run.py
@@ -0,0 +1,61 @@
+import os
+import sys
+
+import requests
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+
+SCREEN_SIZE = [600, 450]
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ll = "37.530887,55.703118"
+        self.spn = "0.002,0.002"
+        self.l = "map"
+
+        self.getImage()
+        self.initUI()
+
+    def getImage(self):
+        map_api_server = "http://static-maps.yandex.ru/1.x/"
+        map_params = {
+            "ll": self.ll,
+            "spn": self.spn,
+            "l": self.l
+        }
+        response = requests.get(map_api_server, params=map_params)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(f"{map_api_server}?{'&'.join([f'{key}={val}' for key, val in map_params.items()])}")
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        # Запишем полученное изображение в файл.
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+    def initUI(self):
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+
+        # Изображение
+        self.pixmap = QPixmap(self.map_file)
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.image.setPixmap(self.pixmap)
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove(self.map_file)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
